9a85170c2e248c48c0dc56752114de47eca76c0e
rendya <60930906+rendya2501@users.noreply.github.com>
2025-04-03 21:54:24 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e  # エラー発生時にスクリプトを終了
 MESSAGE=$1
 echo "Received message: $MESSAGE"
+
+# GitHub Actions の output (outputs に渡す)
+echo "script_result=Script Processed: $MESSAGE" >> "$GITHUB_OUTPUT"
+
+# GitHub Actions の環境変数 (環境変数に設定)
+# echo "env_result=Env Processed: $1" >> "$GITHUB_ENV"
