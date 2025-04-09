0ef79de99af18ae994e1122e71d2e9a9e8a29073
rendya <60930906+rendya2501@users.noreply.github.com>
2025-04-09 13:20:44 +0000

    C#プロジェクト追加

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string result = "Hello from repository B!";
+
+// GitHub Actions の output に値を設定
+string githubOutput = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+if (!string.IsNullOrEmpty(githubOutput))
+{
+    using (StreamWriter writer = new StreamWriter(githubOutput, true))
+    {
+        writer.WriteLine($"csharp_result ={result} : {args[0]}");
+    }
+}
+
+Console.WriteLine($"Set output value: {result}");
